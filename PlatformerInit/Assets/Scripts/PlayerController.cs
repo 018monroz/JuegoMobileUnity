b3fd33efa02060a5692001d88bd437851c0659ca
018monroz <juan.montoyao@upb.edu.co>
2023-05-17 23:36:10 -0500

    Subiendo archivos de Unity al repo
    Se agregan los componentes iniciales con una idea de juego. Plataforma, personaje con sus respectivos movimientos.

---
+++ PlatformerInit/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+
+    Rigidbody2D body;
+    bool jumpable;
+    [SerializeField] float jumpforce = 20;
+    [SerializeField] float speed = 1;
+    public float horizontalInput = 0;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        jumpable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(horizontalInput+"<--");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        if (jumpable && Input.GetKeyDown(KeyCode.Space))
+        {
+            body.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+            jumpable = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) jumpable = true;
+        if (collision.gameObject.CompareTag("Platform")) jumpable = true;
+    }
+
+}
