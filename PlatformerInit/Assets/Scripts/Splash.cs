bf83d5920384ffa522a1552c79075f6b5763cc98
018monroz <juan.montoyao@upb.edu.co>
2023-05-20 15:59:18 -0500

    Pantalla splash

---
+++ PlatformerInit/Assets/Scripts/Splash.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    double init;
+    double end;
+    void Start()
+    {
+        init = Time.timeAsDouble;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        end = Time.timeAsDouble;
+        if(end-init > 5)
+        {
+            SceneManager.CreateScene("MainMenu");
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
